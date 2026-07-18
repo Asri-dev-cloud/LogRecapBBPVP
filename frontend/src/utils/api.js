47ecc796b447fd85ddf526e7fd3e1c92261ed14b
asri hartini <hartiniasri32@gmail.com>
2026-07-19 05:22:31 +0700

    socket beres3

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -9,4 +9,8 @@ const getApiBase = () => {
 };
 
 export const API_BASE = getApiBase();
-export const SOCKET_URL = API_BASE.replace('/api', '');
+// If running on Vercel (where API_BASE defaults to '/api'), connect socket directly to VPS IP
+// since Vercel's rewrite proxies do not support WebSocket connections.
+export const SOCKET_URL = API_BASE === '/api'
+  ? 'http://187.77.126.26:5000'
+  : API_BASE.replace('/api', '');
